8270a64179f82b2ca2b4d151ae0e120e39c4df61
LeoRigasaki <l215635@lhr.nu.edu.pk>
2025-01-24 18:05:55 +0500

    Refactor: switch to pyproject.toml packaging, update .gitignore, remove license, etc.

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name='insightai',  # Changed from insightai
-    version='0.1.0',   # Fresh version number for the new package
-    description='A lightweight library for dataset insights using Groq and OpenAI',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author='Sohaib Ahmed',
-    packages=find_packages(),
-    install_requires=[
-        'openai',
-        'tiktoken',
-        'pandas',
-        'termcolor',
-        'groq',
-        'sqlite3'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-    ],
-)
+setup()
